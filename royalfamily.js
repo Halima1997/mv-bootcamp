71dfe0737857c7d009fe02fc275a75d624d7bece
Halima <61801840+Halima1997@users.noreply.github.com>
2021-06-28 16:10:23 +0100

    Add files via upload

---
+++ royalfamily.js
@@ -0,0 +1,24 @@
+const charles = {
+    firstName: 'Charles'
+    lastName: 'Windsor'
+    parents: []
+}
+
+const diana = {
+    firstName: 'Diana'
+    lastName: 'Wales'
+    parents: []
+}
+
+const harry = {
+    firstName: 'Harry',
+    lastName: 'Windsor',
+    parents[charles, diana],
+    childOf: function () {
+        return this.parents.map(parent => parent.firstName).join(' & ') || "unknown"
+    }
+}
+
+console.log(harry.childOf()) 
+
+
