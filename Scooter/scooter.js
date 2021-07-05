0ca7a4072a407398022311eee217491729618d03
Halima <halima_abdi@hotmail.co.uk>
2021-07-05 12:06:57 +0100

    Assignment

--- Scooter/scooter.js
+++ Scooter/scooter.js
@@ -1,46 +1,48 @@
 class Scooter {
     batteryPercentage;
     qrCode;
-    isBroken=false
+    isBroken = false
 
-constructor(id) {
-    if (!id) {
-        throw new Error('must have an id');
+    constructor(id, qrCode) {
+        if (!id) {
+            throw new Error('must have an id');
+
+        }
+        this.id = id;
+        if (!qrCode) {
+            throw new Error('must scan correct qr code ');
+        }
+        this.qrCode = qrCode;
+
+        this.isBroken = false
+
+
     }
-    this.id=id;
-    this.isBroken = false
+    reportBroken() {
+        isBroken = true
+    }
 
+    charge() {
+        if (!this.isBroken) {
+            return true
+        } else {
+            return false
+        }
+    }
 
-}
-reportBroken () {
-    isBroken = true 
-}
+    reportBroken() {
+        isBroken = true
+    }
 
-charge () {
-    if (!this.isBroken) {
-        return true 
-    } else {
-        return false
+    charge() {
+        if (!this.isBroken) {
+            return true
+        } else {
+            return false
+        }
+
     }
 }
-constructor(qrCode) {
-    if (!qrCode) {
-        throw new Error('must scan correct qr code ');
-    }
-    this.qrCode=qrCode;
-    this.isBroken = false
-}
-reportBroken () {
-    isBroken = true 
-}
 
-charge () {
-    if (!this.isBroken) {
-        return true 
-    } else {
-        return false  
-
-}
-
-module.exports=scooter 
+module.exports = Scooter
 
--- Scooter/scooter.test.js
+++ Scooter/scooter.test.js
@@ -2,13 +2,15 @@ const Scooter = require('./scooter')
 
 describe('Scooter', function() {
     test('is fully charged', function() {
-        const scooter = new Scooter(1);
+        const scooter = new Scooter(1, 2);
         expect(scooter.charge()).toBeTruthy();
 
-    });
+    })
+
 
     test('correct qr code', function() {
-        const scooter = new Scooter(1);
-        expect(scooter.qrCode()).toBeTruthy();
+        const scooter = new Scooter(1, 2);
+        expect(scooter.qrCode).toBe(2);
     
+    });
 })
