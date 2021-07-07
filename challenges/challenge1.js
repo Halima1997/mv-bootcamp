7f9f5580776f273391fa31e4573b64c5bfd55c6d
Halima <halima_abdi@hotmail.co.uk>
2021-07-07 12:03:48 +0100

    challenge

---
+++ challenges/challenge1.js
@@ -0,0 +1,24 @@
+const array = ['apples','pears','bananas'];
+
+const arrayLength = array.length;
+console.log('array length is ' + arrayLength);
+
+const firstElement = array[1]; 
+console.log('first element is ' + firstElement); //will print first element, apples
+
+const array = ['apples','pears','bananas'];
+
+const arrayLength = array.length;
+console.log('array length is ' + arrayLength);
+
+const secondElement = array[1]; 
+console.log('second element is ' + secondElement); //will print second element, pears
+
+const array = ['apples','pears','bananas'];
+
+const arrayLength = array.length;
+console.log('array length is ' + arrayLength);
+
+const thirdElement = array[2]; 
+console.log('third element is ' + thirdElement); //will print third element, bananas
+
